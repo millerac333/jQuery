adfa1c7de35c0d5aa056821cf4c0874b115c53b1
Aaron Miller <millerac333@gmail.com>
2018-06-25 15:50:35 -0500

    fixed syntax and undefined errors

--- songs.js
+++ songs.js
@@ -1,46 +1,38 @@
 $(document).ready(function() {
+
     // Use jQuery to get a reference to `load-songs`
-    $("#load-songs")
+    let $loadSongsList = $("#load-songs")
+    .text("LOAD SONG LIST")
+
     // Use jQuery to get a reference to `song-list`
-    $("#song-list")
+    let $songList = $("#song-list")
     /*
         Attach a click handler to the button with jQuery. When
         the button is clicked, use $.ajax() to load `songs.json`
         from the file system
     */
-    $("#load-songs").click(() => {
-        $.ajax("http://127.0.0.1:8080/songs.json")
-    /*
-        Chain a `.then()` method to the ajax call, and when
-        it is complete build a DOM component for each song with
-        the following structure. Use the jQuery append() method
-        to put an HTML representation of each song the DOM as a
-        child component of the .
-
-            <section class="song">
-                <h1 class="song__title">{Title of song}</h1>
-                <section class="song__description">
-                    Performed by {artist} on the album {album}
-                </section>
-            </section>
-    */
+    $loadSongsList.click(() => {
+        $.ajax("songs.json")
         .then(feedback => {
             console.log(feedback);
-            $("<section>")
-            .addClass("song");
-            $("#song-list").append($("<section>.song"))
+
+            let $songSection = $("<section>")
+            $songSection.addClass("songs");
+            $songList.append($songSection)
+
             feedback.songs.forEach(element => {
                 console.log(element);
-                console.log(songSection);
-                    $("<h1>")
-                    .addClass("song_title")
-                    .text(element.title)
-                    $("<section>.song").append($("<h1>.song_title"))
+                console.log($songSection);
 
-                    $("<section>")
-                    .addClass("song_description")
-                    .text(`Performed by ${element.artist} on the album ${element.album}`)
-                    $("<section>.song").append($("<section>.song_description"))
+                let $songHeader = $("<h1>")
+                $songHeader.addClass("song_title")
+                .text(element.title)
+                $songSection.append($songHeader)
+
+                let $descriptionSection = $("<section>")
+                $descriptionSection.addClass("song_description")
+                .text(`Performed by ${element.artist} on the album ${element.album}`)
+                $songSection.append($descriptionSection)
             });
         })
     })
