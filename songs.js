e2c65286411af5058198d266b647471f3ac438db
Aaron Miller <millerac333@gmail.com>
2018-06-25 12:06:27 -0500

    completed attempt 1

--- songs.js
+++ songs.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-
     // Use jQuery to get a reference to `load-songs`
-
+    $("#load-songs")
     // Use jQuery to get a reference to `song-list`
-
-
+    $("#song-list")
     /*
         Attach a click handler to the button with jQuery. When
         the button is clicked, use $.ajax() to load `songs.json`
         from the file system
     */
-
-
+    $("#load-songs").click(() => {
+        $.ajax("http://127.0.0.1:8080/songs.json")
     /*
         Chain a `.then()` method to the ajax call, and when
         it is complete build a DOM component for each song with
@@ -26,4 +24,24 @@ $(document).ready(function() {
                 </section>
             </section>
     */
+        .then(feedback => {
+            console.log(feedback);
+            $("<section>")
+            .addClass("song");
+            $("#song-list").append($("<section>.song"))
+            feedback.songs.forEach(element => {
+                console.log(element);
+                console.log(songSection);
+                    $("<h1>")
+                    .addClass("song_title")
+                    .text(element.title)
+                    $("<section>").append($("<h4>"))
+
+                    $("<section>")
+                    .addClass("song_description")
+                    .text(`Performed by ${element.artist} on the album ${element.album}`)
+                    $("<section>.song").append($("<section>.song_description"))
+            });
+        })
+    })
 })
